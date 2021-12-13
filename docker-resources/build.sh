dd81b576e7a90e2d9638eed7b5cb4ce1d34a2250
Albert Albala <albert@mediatribe.net>
2021-12-13 18:44:31 -0500

    Avoid dpkg: error processing package libc-bin (--configure) error

--- docker-resources/build.sh
+++ docker-resources/build.sh
@@ -7,9 +7,14 @@ set -e
 curl -sS https://getcomposer.org/installer | php && \
   mv composer.phar /usr/local/bin/composer
 
-apt-get -y update
-apt-get --no-install-recommends -y install git unzip
-rm -rf /var/lib/apt/lists/*
+echo "Must go through hoops to avoid the dreaded 'dpkg: error processing package libc-bin (--configure)' error. See https://github.com/dcycle/prepare-docker-buildx/blob/master/example/Dockerfile"
+apt-get update && \
+  apt-get install -y libc-bin && \
+  apt-get update && \
+  apt-get install -y --no-install-recommends mariadb-client git unzip || true && \
+  dpkg --purge --force-all libc-bin && \
+  apt-get install -y --no-install-recommends mariadb-client git unzip && \
+  rm -rf /var/lib/apt/lists/*
 
 composer global require \
   squizlabs/php_codesniffer \
