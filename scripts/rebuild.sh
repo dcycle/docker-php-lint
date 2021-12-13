ed2441e992de3c754872f8ac2a2905cce2c54f9f
Albert Albala <albert@mediatribe.net>
2021-12-13 17:57:07 -0500

    Provide PHP 8 version as well as PHP 7; prove ARM version as well as AMD version (#3)

--- scripts/rebuild.sh
+++ scripts/rebuild.sh
@@ -2,8 +2,9 @@
 # Rebuild script
 # This is meant to be run on a regular basis to make sure everything works with
 # the latest version of scripts.
-
 set -e
+
+./test.sh
 
 CREDENTIALS="$HOME/.dcycle-docker-credentials.sh"
 
@@ -21,16 +22,31 @@ DATE=`date '+%Y-%m-%d-%H-%M-%S-%Z'`
 MAJORVERSION='2'
 VERSION='2.0'
 
-# Start by getting the latest version of the official drupal image
-docker pull node
-# Rebuild the entire thing
-docker build --no-cache -t dcycle/"$PROJECT":"$VERSION" .
-docker build -t dcycle/"$PROJECT":"$MAJORVERSION" .
-docker build -t dcycle/"$PROJECT":"$MAJORVERSION".$DATE .
-docker build -t dcycle/"$PROJECT":"$VERSION".$DATE .
+# See https://github.com/dcycle/prepare-docker-buildx, for M1 native images.
+git clone https://github.com/dcycle/prepare-docker-buildx.git
+cd prepare-docker-buildx
+export DOCKER_CLI_EXPERIMENTAL=enabled
+./scripts/run.sh
+cd ..
+
+docker buildx create --name mybuilder
+docker buildx use mybuilder
+docker buildx inspect --bootstrap
 docker login -u"$DOCKERHUBUSER" -p"$DOCKERHUBPASS"
-docker push dcycle/"$PROJECT":"$VERSION"
-docker push dcycle/"$PROJECT":"$MAJORVERSION"
-docker push dcycle/"$PROJECT":"$VERSION"."$DATE"
-docker push dcycle/"$PROJECT":"$MAJORVERSION"."$DATE"
-# No longer using the latest tag, use the major version tag instead.
+
+# Start by getting the latest version of the base images
+docker pull php:7
+docker pull php:8
+
+docker buildx build -f="Dockerfile2" -t dcycle/"$PROJECT":"$VERSION" --platform linux/amd64,linux/arm64/v8 --push .
+docker buildx build -f="Dockerfile2" -t dcycle/"$PROJECT":"$MAJORVERSION" --platform linux/amd64,linux/arm64/v8 --push .
+docker buildx build -f="Dockerfile2" -t dcycle/"$PROJECT":"$MAJORVERSION".$DATE --platform linux/amd64,linux/arm64/v8 --push .
+docker buildx build -f="Dockerfile2" -t dcycle/"$PROJECT":"$VERSION".$DATE --platform linux/amd64,linux/arm64/v8 --push .
+
+MAJORVERSION='3'
+VERSION='3.0'
+
+docker buildx build -f="Dockerfile3" -t dcycle/"$PROJECT":"$VERSION" --platform linux/amd64,linux/arm64/v8 --push .
+docker buildx build -f="Dockerfile3" -t dcycle/"$PROJECT":"$MAJORVERSION" --platform linux/amd64,linux/arm64/v8 --push .
+docker buildx build -f="Dockerfile3" -t dcycle/"$PROJECT":"$MAJORVERSION".$DATE --platform linux/amd64,linux/arm64/v8 --push .
+docker buildx build -f="Dockerfile3" -t dcycle/"$PROJECT":"$VERSION".$DATE --platform linux/amd64,linux/arm64/v8 --push .
