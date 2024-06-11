79c13e103647b0269be359fc4b8ef1404b8308f9
SKN <skn@SKNs-MacBook-Pro.local>
2024-06-11 22:33:55 +0530

    updated rebuild script

--- scripts/install-docker-and-rebuild.sh
+++ scripts/install-docker-and-rebuild.sh
@@ -17,10 +17,7 @@ echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt-get update
-sudo apt-get -y install docker-ce docker-ce-cli containerd.io docker-compose-plugin
-
-echo '=> Installing Docker Compose.'
-sudo apt-get -y install docker-compose
+sudo apt-get -y install docker-ce docker-ce-cli containerd.io
 
 echo '=> Rebuild.'
 ./scripts/rebuild.sh
